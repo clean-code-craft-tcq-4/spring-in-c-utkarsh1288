c4bc6b51a76fd7392646cbd6a9c8825b9f2dbc2d
utkarsh1288 <105204517+utkarsh1288@users.noreply.github.com>
2022-05-12 14:45:47 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,8 +1,28 @@
-
-struct Stats compute_statistics(const float* numberset, int setlength);
-
-typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+#include <math.h>
+#ifdef NAN
+/*NAN is supported*/
+#endif
+struct Stats
+{
+    float average;
+    float min;
+    float max;
+};
 
 extern int emailAlertCallCount;
+
 extern int ledAlertCallCount;
+
+extern int NAN_Detected =0;
+
+typedef void (*alerter_funcptr)();
+
+void emailAlerter();
+
+void ledAlerter();
+
+extern void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+
+extern struct Stats compute_statistics(const float* numberset, int setlength);
+
+
